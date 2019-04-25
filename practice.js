97871d385f69f6c2c15bec5ff96f65cbabf99c5c
Daria <dariaurbanik@gmail.com>
2019-04-25 15:19:32 +0200

    Function sum added and called

--- practice.js
+++ practice.js
@@ -99,12 +99,14 @@ function nameCheck (name) {
 // that will be numbers.
 // The add function should return the two parameters added together
 
-//Code Here
+function add(num1, num2){
+  return num1+num2
+}
 
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
-//Code Here
+let mathSum = add(3, 4)
 
 //////////////////PROBLEM 12////////////////////
 
