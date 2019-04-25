bd81059d130ad0a5f92446892366187a27c55399
Daria <dariaurbanik@gmail.com>
2019-04-25 15:17:35 +0200

    Function nameCheck created

--- practice.js
+++ practice.js
@@ -83,7 +83,15 @@ let devMountainClassPet = dog.name
 // If the name parameter is anything else, return 'Cool name, NAMEPARAM'
 // with NAMEPARAM being the name parameter being passed in
 
-// Code here
+function nameCheck (name) {
+  if (name === 'Steven'){
+    return 'What is up Steven?'
+  } else if (name === 'Bryan') {
+    return 'Hey Bryan!'
+  } else {
+    return 'Cool name, ' + name
+  }
+}
 
 //////////////////PROBLEM 11////////////////////
 
